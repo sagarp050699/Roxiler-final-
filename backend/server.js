a4a9207c7f027b6000af243612dc60cdeca6477d
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-11-07 15:31:47 +0530

    removed __dirname

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,10 @@ app.get("/", (req, res) => {
 });
 
 // production
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.use(express.static(path.join(path.resolve(), "/frontend/dist")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+  res.sendFile(path.join(path.resolve(), "frontend", "dist", "index.html"))
 );
 
 app.listen(process.env.PORT, () => {
